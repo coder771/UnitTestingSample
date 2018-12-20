ee2b4cd39477ecf25c4c4eebc76a3eb7e0725536
ChristianY <ChristianY@MS-W105>
2018-12-20 15:20:10 +0000

    initial commit

---
+++ UnitTestingExample/UnitTestingSample/UnitTestingSample.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestingSample.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var Calculator = new Calculator();
+            int result = Calculator.Add(10, 2);
+            Assert.AreEqual(12, result);
+
+        }
+    }
+}
---
+++ UnitTestingExample/UnitTestingSample/UnitTestingSample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var calculator = new Calculator();
+            calculator.Add(10, 2);
+        }
+    }
+}
